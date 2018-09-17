0d9c49ce6bb9d7a467419ffe7da4634ab9a7a771
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-17 23:53:43 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, 0, character = "X")
-input_to_index(user_input)
-board[index] = character
+def move(board, input, character = "X")
+user_input = input_to_index(user_input)
+input = user_input
+board[input] = character
 end
